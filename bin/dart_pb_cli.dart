db34bfbbb05d4b26103bda6fbf06a407c2620afa
getBoolean <getboolean@gmail.com>
2022-12-12 00:50:15 -0600

    Initial commit

---
+++ bin/dart_pb_cli.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pb_cli/dart_pb_cli.dart' as dart_pb_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pb_cli.calculate()}!');
+}
---
+++ test/dart_pb_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pb_cli/dart_pb_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
