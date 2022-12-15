6029ba40c9a66181728d10bb52d510f019b67286
getBoolean <getboolean@gmail.com>
2022-12-15 03:21:28 -0600

    starting cli with help command

--- bin/dpaperback.dart
+++ bin/dpaperback.dart
@@ -1,13 +1,110 @@
 import 'dart:io';
 
-import 'package:args/args.dart';
+import 'package:dcli/dcli.dart';
 import 'package:dpaperback_cli/dpaperback_cli.dart';
 
 void main(List<String> arguments) {
   exitCode = 0; // presume success
-  final parser = ArgParser();
+  final bundleParser = ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Print this usage information.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      defaultsTo: false,
+      negatable: false,
+      help: 'Enable verbose logging.',
+    );
+  final serveParser = ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Print this usage information.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      defaultsTo: false,
+      negatable: false,
+      help: 'Enable verbose logging.',
+    );
+  final cleanParser = ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Print this usage information.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      defaultsTo: false,
+      negatable: false,
+      help: 'Enable verbose logging.',
+    );
+  final baseParser = ArgParser(allowTrailingOptions: false)
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Print this usage information.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      defaultsTo: false,
+      negatable: false,
+      help: 'Enable verbose logging.',
+    )
+    ..addCommand('bundle', bundleParser)
+    ..addCommand('serve', serveParser)
+    ..addCommand('clean', cleanParser);
 
-  final ArgResults argResults = parser.parse(arguments);
+  final ArgResults results = baseParser.parse(arguments);
 
-  dpaperback();
+  final verbose = results['verbose'] as bool;
+  final help = results['help'] as bool;
+
+  final command = results.command;
+  if (help && command == null) {
+    printUsage(
+      usage: baseParser.usage,
+      verbose: verbose,
+    );
+    exit(0);
+  }
+
+  if (command == null) {
+    print(red('\nYou must pass the name of the command to use.'));
+    printUsage(
+      usage: baseParser.usage,
+      verbose: verbose,
+    );
+    exit(1);
+  }
+
+  final enableVerboseLogging = verbose || command['verbose'] as bool;
+  final shouldShowHelp = help || command['help'] as bool;
+
+  if (shouldShowHelp) {
+    printCommandUsage(
+      command: command,
+      verbose: verbose,
+      usage: command.name == 'bundle'
+          ? bundleParser.usage
+          : command.name == 'serve'
+              ? serveParser.usage
+              : command.name == 'clean'
+                  ? cleanParser.usage
+                  : baseParser.usage,
+    );
+    exit(0);
+  }
+
+  dpaperback(command, verbose: enableVerboseLogging);
 }
--- lib/dpaperback_cli.dart
+++ lib/dpaperback_cli.dart
@@ -1,3 +1,22 @@
-void dpaperback() {
+import 'package:dcli/dcli.dart';
+
+void dpaperback(ArgResults command, {required bool verbose}) {
   print('Hello, World!');
+}
+
+void printUsage({bool verbose = false, required String usage}) {
+  print(green('\nUsage: dpaperback <command> [arguments]'));
+  print(green('\nGlobal options:'));
+  print(green(usage));
+  print(green('\nAvailable commands:'));
+  print(green(
+      '   bundle          Builds all the sources in the repository and generates a versioning file'));
+  print(green('   serve           Build the sources and start a local server'));
+  print(green('   clean           Deletes the modules directory and the versioning file'));
+}
+
+void printCommandUsage({required ArgResults command, bool verbose = false, required String usage}) {
+  print(green('\nUsage: dpaperback ${command.name} [arguments]'));
+  print(green('\nOptions:'));
+  print(green(usage));
 }
