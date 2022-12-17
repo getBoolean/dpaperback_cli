95f7c8c1b228495cf324cd5228b3a789cbddade8
getBoolean <getboolean@gmail.com>
2022-12-17 11:33:18 -0600

    Update dpaperback_cli.dart

--- lib/dpaperback_cli.dart
+++ lib/dpaperback_cli.dart
@@ -1,5 +1,5 @@
 export 'src/commands/bundle.dart';
 export 'src/commands/clean.dart';
-export 'src/time_mixin.dart';
 export 'src/commands/server.dart';
 export 'src/dpaperback_command_runner.dart';
+export 'src/time_mixin.dart';
