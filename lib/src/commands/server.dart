e629e417d8c1c8bdb999981b61098fc79a877f29
getBoolean <getboolean@gmail.com>
2022-12-17 22:23:02 -0600

    Add server implementation

--- lib/src/commands/server.dart
+++ lib/src/commands/server.dart
@@ -1,19 +1,25 @@
-import 'dart:io';
+import 'dart:io' as io;
 
 import 'package:args/command_runner.dart';
 import 'package:dcli/dcli.dart';
+import 'package:dpaperback_cli/src/commands/bundle.dart';
 import 'package:dpaperback_cli/src/time_mixin.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
 
 class Server extends Command<int> {
   final ProviderContainer container;
   Server([ProviderContainer? container]) : container = container ?? ProviderContainer() {
     argParser
       ..addSeparator('Flags:')
+      ..addFlag('skip-bundle', defaultsTo: false)
       ..addOption('output',
           abbr: 'o', help: 'The output directory.', defaultsTo: './', valueHelp: 'folder')
       ..addOption('target',
           abbr: 't', help: 'The directory with sources.', defaultsTo: 'lib', valueHelp: 'folder')
+      ..addOption('source', abbr: 's', help: 'Bundle a single source.', valueHelp: 'source name')
       ..addOption(
         'paperback-extensions-common',
         abbr: 'c',
@@ -22,7 +28,7 @@ class Server extends Command<int> {
         defaultsTo: kDefaultPaperbackExtensionsCommon,
       )
       ..addOption('ip', valueHelp: 'value')
-      ..addOption('port', abbr: 'p', defaultsTo: '27015', valueHelp: 'value');
+      ..addOption('port', abbr: 'p', defaultsTo: '8080', valueHelp: 'value');
   }
   @override
   String get description => 'Build the sources and start a local server';
@@ -41,15 +47,27 @@ class Server extends Command<int> {
     final output = await parseOutputPath(results);
     final target = parseTargetPath(results);
     final commonsPackage = results['paperback-extensions-common'] as String;
-    final port = results['port'];
-
-    return ServerCli(
+    final int port = int.parse(results['port']);
+    final source = results['source'];
+    final skipBundle = results['skip-bundle'] as bool;
+    if (!skipBundle) {
+      await BundleCli(
+        output: output,
+        target: target,
+        source: source,
+        commonsPackage: commonsPackage,
+        container: container,
+      ).run();
+    }
+    final successCode = ServerCli(
       output: output,
       target: target,
       port: port,
       commonsPackage: commonsPackage,
       container: container,
     ).run();
+
+    return successCode;
   }
 
   String parseTargetPath(ArgResults command) {
@@ -57,7 +75,7 @@ class Server extends Command<int> {
     final targetPath = canonicalize(targetArgument);
     if (!exists(targetPath)) {
       print(red('The target directory "$targetArgument" could not be found'));
-      exit(2);
+      io.exit(2);
     }
 
     return targetPath;
@@ -68,7 +86,7 @@ class Server extends Command<int> {
     final outputPath = canonicalize(outputArgument);
 
     if (!exists(outputPath)) {
-      await Directory(outputPath).create(recursive: true);
+      await io.Directory(outputPath).create(recursive: true);
     }
     return outputPath;
   }
@@ -89,8 +107,18 @@ class ServerCli with CommandTime {
     required this.container,
   });
 
-  int run() {
-    print(blue('\nStarting Server on port $port'));
+  Future<int> run() async {
+    final bundlesPath = join(output, 'bundles');
+    final pipeline = const shelf.Pipeline()..addMiddleware(shelf.logRequests());
+    final handler = pipeline.addHandler(
+      createStaticHandler(bundlesPath,
+          /*defaultDocument: 'versioning.json', */ listDirectories: true),
+    );
+
+    await shelf_io.serve(handler, 'localhost', port).then((server) {
+      print(green('\nStarting server on at http://${server.address.host}:${server.port}'));
+    });
+
     return 0;
   }
 }
