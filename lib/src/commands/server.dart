39b649265e5e61c9229b50c89b09a5fdc1415b94
getBoolean <getboolean@gmail.com>
2022-12-17 23:24:03 -0600

    Add help and quit to server (restart not working)

--- lib/src/commands/server.dart
+++ lib/src/commands/server.dart
@@ -1,4 +1,5 @@
 import 'dart:io' as io;
+import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:dcli/dcli.dart';
@@ -28,7 +29,8 @@ class Server extends Command<int> {
         defaultsTo: kDefaultPaperbackExtensionsCommon,
       )
       ..addOption('ip', valueHelp: 'value')
-      ..addOption('port', abbr: 'p', defaultsTo: '8080', valueHelp: 'value');
+      ..addOption('port', abbr: 'p', defaultsTo: '8080', valueHelp: 'value')
+      ..addOption('host', valueHelp: 'ip-address', help: 'Override the host address');
   }
   @override
   String get description => 'Build the sources and start a local server';
@@ -48,6 +50,7 @@ class Server extends Command<int> {
     final target = parseTargetPath(results);
     final commonsPackage = results['paperback-extensions-common'] as String;
     final int port = int.parse(results['port']);
+    final String? host = results['host'];
     final source = results['source'];
     final skipBundle = results['skip-bundle'] as bool;
     if (!skipBundle) {
@@ -62,7 +65,9 @@ class Server extends Command<int> {
     final successCode = ServerCli(
       output: output,
       target: target,
+      source: source,
       port: port,
+      host: host,
       commonsPackage: commonsPackage,
       container: container,
     ).run();
@@ -95,14 +100,18 @@ class Server extends Command<int> {
 class ServerCli with CommandTime {
   final String output;
   final String target;
+  final String? source;
   final String commonsPackage;
   final int port;
+  final String? host;
   final ProviderContainer container;
 
   ServerCli({
     required this.output,
     required this.target,
+    this.source,
     required this.port,
+    required this.host,
     required this.commonsPackage,
     required this.container,
   });
@@ -111,14 +120,52 @@ class ServerCli with CommandTime {
     final bundlesPath = join(output, 'bundles');
     final pipeline = const shelf.Pipeline()..addMiddleware(shelf.logRequests());
     final handler = pipeline.addHandler(
-      createStaticHandler(bundlesPath,
-          /*defaultDocument: 'versioning.json', */ listDirectories: true),
+      createStaticHandler(bundlesPath, defaultDocument: 'versioning.json', listDirectories: true),
     );
+    // await IpAddress().getIpAddress()
+    final server = await shelf_io.serve(handler, host ?? '127.0.0.1', port);
+    print(green('\nStarting server on at http://${server.address.host}:${server.port}'));
 
-    await shelf_io.serve(handler, 'localhost', port).then((server) {
-      print(green('\nStarting server on at http://${server.address.host}:${server.port}'));
-    });
+    var stopServer = false;
+    while (!stopServer) {
+      final input = ask(prefixTime(), required: false).trim();
+
+      if (input == 'h' || input == 'help') {
+        print('Help');
+        print('  h, help - Display this message');
+        print('  s, stop - Stop the server');
+        print('  r, restart - Restart the server, also rebuilds the sources');
+      }
+
+      if (input == 's' || input == 'stop' || input == 'exit' || input == 'quit' || input == 'q') {
+        stopServer = true;
+        exit(0);
+      }
+
+      if (input == 'r' || input == 'restart') {
+        print(blue('Building Sources'));
+
+        // Make sure the repo is bundled
+        await BundleCli(
+          output: output,
+          target: target,
+          source: source,
+          commonsPackage: commonsPackage,
+          container: container,
+        ).run();
+        print(blue('\nStarting Server on port $server.port'));
+
+        print('\nFor a list of commands do ${green('h')} or ${green('help')}');
+      }
+    }
 
     return 0;
   }
+}
+
+String prefixTime() {
+  final now = DateTime.now();
+  final time =
+      '[${now.hour}:${now.minute}:${now.second}:${now.millisecond.toString().padLeft(4, '0')}] :';
+  return grey(time);
 }
